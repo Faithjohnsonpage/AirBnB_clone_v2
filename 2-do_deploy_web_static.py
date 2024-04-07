49aa099b0651a699d27da7dc4c6d9c300e5aa124
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-07 12:28:34 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This Fabric script distributes an archive to your web servers,
-using the function do_deploy:"""
-from fabric.api import *
+using the function do_deploy."""
+from fabric.api import run, put, env
 from datetime import datetime
 import os
 
@@ -11,7 +11,6 @@ env.hosts = ['34.207.63.80', '54.208.120.30']
 env.user = 'ubuntu'
 env.key = '~/.ssh/id_rsa'
 
-
 def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
     # Check if `archive_path` exists
@@ -20,13 +19,18 @@ def do_deploy(archive_path):
 
     try:
         # upload archive
-        put('archive_path', '/tmp/')
+        put(archive_path, '/tmp/')
 
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         folder = f"/data/web_static/releases/web_static_{timestamp}"
 
-        run('tar -xzvf /tmp/{archive_path} -C {}'.format(folder))
-        run('rm -r archive_path')
+        # Create target directory if it doesn't exist
+        run(f'mkdir -p {folder}')
+
+        # Uncompress archive
+        run(f'tar -xzvf /tmp/{archive_path} -C {folder}')
+        # Remove uploaded archive
+        run(f'rm /tmp/{archive_path}')
 
         # Remove the existing symbolic link
         run('rm -rf /data/web_static/current')
